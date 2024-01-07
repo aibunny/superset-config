6d7e45f3ad909e81d71d4beae041e3945ff83b08
aibunny <waihenyafredrick@gmail.com>
2024-01-07 13:44:18 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM apache/superset:latest
 
 # Install PostgreSQL driver
 RUN pip install psycopg2-binary
+RUN pip install python-dotenv
+
+COPY --chown=superset superset_config.py /app/
+ENV SUPERSET_CONFIG_PATH /app/superset_config.py
 
 # Expose the Superset web port
 EXPOSE 8000
---
+++ superset_config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Superset specific config
+ROW_LIMIT = 5000
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+# Flask-WTF flag for CSRF
+WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
+
+# Set this API key to enable Mapbox visualizations
+MAPBOX_API_KEY = ''
