5b7472bff8c13b8d5b34cc2ce2b9b044eb71c912
aibunny <waihenyafredrick@gmail.com>
2024-01-08 11:36:46 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,22 @@
 # Use the official Superset image from Docker Hub
 FROM apache/superset:2.1.3
 
+# Switching to root to install additional dependencies
 USER root
 
 # Install PostgreSQL driver
-RUN pip install psycopg2-binary
-RUN pip install python-dotenv
-
-COPY . /app/
+RUN pip install psycopg2-binary gunicorn python-dotenv gevent
 
 # Switching back to using the `superset` user
 USER superset
 
-# config file
+# Copying additional files and configurations
+COPY .env /app/pythonpath/.env
+
+# Adding your custom Superset configuration
 COPY superset_config.py /app/pythonpath/superset_config.py
 
+
 # Copy logo image file
-COPY ./image/logo.png /app/superset/static/assets/images/miracleplus-logo.png
-
-
-# Expose the Superset web port
-EXPOSE 8000
-
-
-# Start Superset
-CMD ["superset", "run", "-p", "8000", "--with-threads", "--reload", "--no-debugger"]
-
+COPY ./image/logo.webp /app/superset/static/assets/images/logo.webp
 
--- superset_config.py
+++ superset_config.py
@@ -8,12 +8,20 @@ ROW_LIMIT = 5000
 
 SUPERSET_SECRET_KEY="$(openssl rand -base64 42)"
 SECRET_KEY="$(openssl rand -base64 42)"
+
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
 # Add endpoints that need to be exempt from CSRF protection
 WTF_CSRF_EXEMPT_LIST = []
 # A CSRF token that expires in 1 year
 WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
+
+
+FLASK_ENV = 'production'
+
+FLASK_DEBUG = False
+
+
 
 # Set this API key to enable Mapbox visualizations
 MAPBOX_API_KEY = ''
@@ -37,8 +45,8 @@ SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
     DATABASE_DB,
 )
 
-# Don't forget copy file in Dockerfile, destination: /app/superset/static/assets/images/miracleplus-logo.png
-# APP_ICON = "/static/assets/images/miracleplus-logo.png"
+# # Don't forget copy file in Dockerfile, destination: /app/superset/static/assets/images/logo.webp
+# APP_ICON = "/static/assets/images/logo.webp"
 # APP_ICON_WIDTH = 38
 
 LANGUAGES = {
@@ -50,4 +58,25 @@ LANGUAGES = {
 FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True
     #Jinja SQL ：https://superset.apache.org/docs/installation/sql-templating/ 
+}
+
+BROKER_URL = 'redis://redis:6379/0'
+
+#charting data queried from connected datasources (DATA_CACHE_CONFIG).
+DATA_CACHE_CONFIG = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24,  # 1 day default (in secs)
+    'CACHE_KEY_PREFIX': 'Aibunny-Config',
+    'CACHE_REDIS_URL': os.getenv("REDIS_URL"),
+}
+
+TALISMAN_ENABLED =os.getenv("TALISMAN_ENABLED", False)
+
+# Configure CSP policies
+TALISMAN_CONFIG = {
+    'content_security_policy': {
+        'default-src': '\'self\'',
+        'img-src': '*',
+       
+    },
 }
