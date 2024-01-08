9d973a308ce54ef02794ec752e1a2807d51bcf31
aibunny <waihenyafredrick@gmail.com>
2024-01-08 09:40:06 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,20 @@ RUN pip install psycopg2-binary
 RUN pip install python-dotenv
 
 COPY . /app/
-ENV SUPERSET_CONFIG_PATH /app/superset_config.py
+
+# Switching back to using the `superset` user
+USER superset
+
+# config file
+COPY superset_config.py /app/pythonpath/superset_config.py
+
+# Copy logo image file
+COPY ./image/logo.png /app/superset/static/assets/images/miracleplus-logo.png
+
 
 # Expose the Superset web port
 EXPOSE 8000
 
-# Switching back to using the `superset` user
-USER superset
 
 # Start Superset
 CMD ["superset", "run", "-p", "8000", "--with-threads", "--reload", "--no-debugger"]
--- superset_config.py
+++ superset_config.py
@@ -6,10 +6,8 @@ load_dotenv()
 # Superset specific config
 ROW_LIMIT = 5000
 
-SUPERSET_SECRET_KEY= os.getenv("SUPERSET_SECRET_KEY")
-
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
+SUPERSET_SECRET_KEY="$(openssl rand -base64 42)"
+SECRET_KEY="$(openssl rand -base64 42)"
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
 # Add endpoints that need to be exempt from CSRF protection
@@ -19,3 +17,37 @@ WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
 
 # Set this API key to enable Mapbox visualizations
 MAPBOX_API_KEY = ''
+
+DATABASE_DIALECT = os.getenv("DATABASE_DIALECT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_DB = os.getenv("DATABASE_DB")
+
+
+
+
+SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
+    DATABASE_DIALECT,
+    DATABASE_USER,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_DB,
+)
+
+# Don't forget copy file in Dockerfile, destination: /app/superset/static/assets/images/miracleplus-logo.png
+# APP_ICON = "/static/assets/images/miracleplus-logo.png"
+# APP_ICON_WIDTH = 38
+
+LANGUAGES = {
+    'en': {'flag': 'us', 'name': 'English'},
+}
+
+#TODO: Add redis cache
+
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True
+    #Jinja SQL ：https://superset.apache.org/docs/installation/sql-templating/ 
+}
